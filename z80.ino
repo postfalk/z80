896d536a265abc780213664d1ae1e843b38c7eb6
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2015-10-22 22:07:55 -0700

    speeding up Arduino code by factor 15

--- z80.ino
+++ z80.ino
@@ -7,43 +7,59 @@ int spd = 0; //clock speed in ms delays
 
 void reset() {
   // reset z80 (four cycles);
-  digitalWrite(A3, LOW);
-  for (int i=0; i<5; i++) {
-    digitalWrite(A5, HIGH);
+  PORTC = PORTC ^ B00000000;
+  for (int i=0; i<10; i++) {
+    PORTC = PORTC ^ B00100000;
+    // Serial.println(PORTC | B10000000, BIN);
     delay(50);
-    digitalWrite(A5, LOW);
+    PORTC = PORTC ^ B00100000;
+    // Serial.println(PORTC | B10000000, BIN);
     delay(50);
   }
-  digitalWrite(A3, HIGH);
+  PORTC = PORTC | B00001000;
 }
 
 void setup() {
+  // Serial.begin(9600);
   // use C (analog in) port as Control Bus
   // clock
-  pinMode(A5, OUTPUT);
+  // pinMode(A5, OUTPUT);
   // rd
-  pinMode(A4, INPUT);
+  // pinMode(A4, INPUT);
   // reset
-  pinMode(A3, OUTPUT);
+  // pinMode(A3, OUTPUT);
   // mreq
-  pinMode(A2, INPUT);
+  // pinMode(A2, INPUT);
   // use D port as Data bus
+  // input only simulating ROM
   DDRD = B11111111;
   PORTD = B00000000;
   // use B port as Address bus
   // which has six lines available
   // 64 byte address space
-  DDRB = B00000000; 
+  DDRB = B00000000;
+  DDRC = B00101000;
+  // PORTC = PORTC | B00100000;
   reset();
 }
 
 void loop() {
-  digitalWrite(A5, HIGH);
+  // delay(100);
+  PORTC = PORTC | B00100000;
+  // Serial.println(PORTC | B10000000, BIN);
+  // delay(100);
+  PORTC = PORTC ^ B00100000;
+  // Serial.println(PORTC | B10000000, BIN);
+  // digitalWrite(A5, HIGH);
   // delay(spd);
-  digitalWrite(A5, LOW);
-  rd = digitalRead(A4);
-  mreq = digitalRead(A2);
-  if (!rd && !mreq) {
+  // digitalWrite(A5, LOW);
+  // rd = digitalRead(A4);
+  // mreq = digitalRead(A2);
+  // delay(100);
+  // Serial.println(PINC, BIN);
+  // Serial.println(PINB);
+  // Serial.println(PINC & B0000010, BIN);
+  if (!(PINC & B00010100)) {
     DDRD = B11111111;
     PORTD = code[PINB];
   }
