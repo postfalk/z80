cf8e9663abe96a0e378d22b4eb22e2e6b307e979
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2015-10-25 11:10:26 -0700

    additional cleanup, remove debug code

--- z80.ino
+++ z80.ino
@@ -6,23 +6,21 @@ int spd = 0; //clock speed in ms delays
 
 void reset() {
   // reset z80 (four cycles);
-  PORTC = PORTC ^ B00000000;
+  // set reset to low
+  PORTC = PORTC & B11110111;
+  // change clock 10 times (equals 5 cycles)
   for (int i=0; i<10; i++) {
     PORTC = PORTC ^ B00100000;
-    // Serial.println(PORTC | B10000000, BIN);
-    delay(50);
-    PORTC = PORTC ^ B00100000;
-    // Serial.println(PORTC | B10000000, BIN);
-    delay(50);
   }
+  // set reset to high
   PORTC = PORTC | B00001000;
 }
 
 void setup() {
   // use D port as Data bus
-  // input only simulating ROM
-  DDRD = B11111111;
-  PORTD = B00000000;
+  // output only simulating ROM
+  // however set inactive/INPUT For now
+  DDRD = B00000000;
   // use B port as Address bus
   // which has six lines available
   // 64 byte address space
@@ -33,18 +31,23 @@ void setup() {
   // 4 read <- in
   // 5 clock -> out
   DDRC = B00101000;
-  // PORTC = PORTC | B00100000;
+  // call reset
   reset();
 }
 
 void loop() {
   PORTC = PORTC | B00100000;
+  // clock high phase, a good space for debugging
   PORTC = PORTC ^ B00100000;
   if (!(PINC & B00010100)) {
+    // set Port D as output 
     DDRD = B11111111;
+    // fetch code
     PORTD = code[PINB];
   }
   else {
+    // set PORT D to input state in order not to 
+    // interfere with other data bus operations
     DDRD = B00000000;
   }
 }
