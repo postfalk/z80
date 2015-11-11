ffeb2119aac47b1015165892b91aaadcb75b019b
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2015-11-11 15:19:36 -0800

    first pass on a byte parser

---
+++ piomodified.asm
@@ -0,0 +1,42 @@
+org 00000h
+
+setup:  ld sp, 0ffffh
+        ld a,00fh
+        out (003h),a
+
+loop:   nop
+        ld a, 010101110b
+        ld hl, 0ff00h   ; pass argument through random memory location
+        ld (hl), a
+        call parse
+        jp loop
+
+parse:  nop             ; parse a byte from a register
+        ld b, 008h      ; for i=0 to 7
+lp1:    ld a, (hl)
+        rlca
+        ld (hl), a
+        jr nc, off
+        ld a, 001h
+        jr cont
+off:    ld a, 000h
+cont:   out (002h), a
+        push bc
+        ld bc, 0100h
+        call wait
+        pop bc
+        dec b
+        jp nz, lp1
+        ret
+
+wait:   nop             ; set relative wait time in bc before calling
+outer:  ld de, 0100h
+inner:  dec de
+        ld a, d
+        or e
+        jp nz, inner
+        dec bc
+        ld a, b
+        or c
+        jp nz, outer
+        ret
