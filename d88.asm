bf842359aeee6db155e40cc1ec3523943e53ab97
Falk Schuetzenmeister <falk_email@yahoo.com>
2016-01-08 20:59:07 -0800

    cleanup and renaming

--- d88.asm
+++ d88.asm
@@ -12,7 +12,7 @@
 ; TODO; streamline!
 
 org 00000h
-display:    db 080h, 055h, 077h, 055h, 055h, 055h, 055h, 055h
+display:    db 001h, 055h, 077h, 055h, 055h, 055h, 055h, 055h
 
 setup:      ld sp, 0ffffh   ; set stack pointer
             ld hl, 00ff00h  ; begin page to store datasheet
@@ -32,116 +32,115 @@ oscillate:  ld a, 000h      ; there has to be a clock signal
             call output
             ret
 
-i2cmessage: call startcond
-            call address
-            ; start oscillator
-            ld a, 021h
-            call parse
-            call endcond
-            ; set brightness
-            call startcond
-            call address
-            ld a, 0e0h
-            call parse
-            call endcond
-            ; turn display on
-            call startcond
-            call address
-            ld a, 081h
-            call parse
-            call endcond
-            call startcond
-            call address
-            ld a, 000h
-            call parse
-            ; write data
-            ld b, 08h
-lp10:       ld a, (display)
-            call parse
-            dec b
-            jp nz, lp10
-            call endcond
-            ret
+i2cmessage:         call startTransmission
+                    ; start oscillator
+                    ld a, 021h
+                    call write
+                    call endTransmission
+                    ; set brightness
+                    call startTransmission
+                    ld a, 0e0h
+                    call write
+                    call endTransmission
+                    ; turn display on
+                    call startTransmission
+                    ld a, 081h
+                    call write
+                    call endTransmission
+                    call startTransmission
+                    ld a, 000h
+                    call write
+                    ; write data
+                    ld b, 0fh
+    lp10:           ld a, (display)
+                    call write
+                    dec b
+                    jp nz, lp10
+                    call endTransmission
+                    ret
 
-address:    ld a, 0e0h      ; call device address
-            call parse
-            ret
+; use terminology similar to Wire.h C-library
+startTransmission:  call startcond
+                    call address
+                    ret
 
-startcond:  ld a,003h
-            call output
-            ld bc, 00001h   ; wait here to make start condition stable, could be much shorter
-            call wait
-            ld a,002h
-            call output
-            ret
+address:            ld a, 0e0h      ; call device address
+                    call write
+                    ret
 
-skip:       ld a,000h
-            call parse
-            ret
+startcond:          ld a,003h
+                    call output
+                    ld bc, 00001h   ; wait here to make start condition stable, could be much shorter
+                    call wait
+                    ld a,002h
+                    call output
+                    ret
 
-endcond:    ld a, 000h
-            call output
-            ld a, 002h
-            call output
-            ld a, 003h
-            call output
-            ret
+skip:               ld a,000h
+                    call write
+                    ret
 
-parse:      push bc
-            ld b, 008h      ; for i=0 to 7
-            ld c, a         ; using c here since I have to push bc anyways
-lp1:        rlc c           ; rotate to left so that most significant bit becomes least significant
-            ld a, 001h      ; load mask
-            and c           ; and deletes all not masked bits
-            call clbit
-            dec b
-            jp nz, lp1
-            call ackn
-            pop bc
-            ret
+endTransmission:    ld a, 000h
+                    call output
+                    ld a, 002h
+                    call output
+                    ld a, 003h
+                    call output
+                    ret
+
+write:              push bc
+                    ld b, 008h      ; for i=0 to 7
+                    ld c, a         ; using c here since I have to push bc anyways
+    lp1:            rlc c           ; rotate to left so that most significant bit becomes least significant
+                    ld a, 001h      ; load mask
+                    and c           ; and deletes all not masked bits
+                    call clbit
+                    dec b
+                    jp nz, lp1
+                    call ackn
+                    pop bc
+                    ret
 
 ; clock cycle around bit, from register a
 ; removed getting back to original state
 ; seems to work without it
-clbit:      push bc
-            call output
-            or 002h
-            call output
-            pop bc
-            ret
+clbit:              push bc
+                    call output
+                    or 002h
+                    call output
+                    pop bc
+                    ret
 
 ; send acknowledge sequence
 ; TODO: actually acknowledge and not
 ; just assume ok
-ackn:       ld a, 001h
-            call output
-            ld a, 003h
-            call output
-            ld a, 001h
-            call output
-            ret
+ackn:               ld a, 001h
+                    call output
+                    ld a, 003h
+                    call output
+                    ret
 
 ; simulate open drain by switching 
 ; between PIOs output modes
 ; see http://members.iinet.net.au/~daveb/downloads/Z80.zip
-output:     ld c, 003h
-            ld b, 0cfh
-            out(c), b
-            out(c), a
-            ret
+output:             ld c, 003h
+                    ld b, 0cfh
+                    out(c), b
+                    out(c), a
+                    ret
 
 ; set relative wait time in bc
-wait:       push de         ; protect affected registers
-            push af
-outer:      ld de, 0100h
-inner:      dec de
-            ld a, d
-            or e
-            jp nz, inner
-            dec bc
-            ld a, b
-            or c
-            jp nz, outer
-            pop af
-            pop de
-            ret
+wait:               push de         ; protect affected registers
+                    push af
+    outer:          ld de, 0100h
+        inner:      dec de
+                    ld a, d
+                    or e
+                    jp nz, inner
+                    dec bc
+                    ld a, b
+                    or c
+                    jp nz, outer
+                    pop af
+                    pop de
+                    ret
