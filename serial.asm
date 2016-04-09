44688b40fb1859844cec7c5871c623254acfa027
Falk Schuetzenmeister <falk_email@yahoo.com>
2016-04-08 21:06:51 -0700

    add basic serial output implementation

--- serial.asm
+++ serial.asm
@@ -1,161 +1,45 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                                                        ;
-;  This is a software implementation of I2C for my little Z80 project    ;
-;  ==================================================================    ;
-;  It currently drives Adafruits LED backpacks based on ht16k33 chip     ;
-;                                                                        ;
-;  See datasheet here                                                    ;
-;  http://www.adafruit.com/datasheets/ht16K33v110.pdf                    ;
-;                                                                        ;
+;  Basic serial output implementation
+;
+;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO; streamline!
 
 org 00000h
 
 setup:              ld sp, 0ffffh       ; set stack pointer
-                    ld hl, 00ff00h      ; begin page to store datasheet
-                    ld a, 00fh          ; set PIO B to output mode
-                    out (03h),a         ;
-                    ld a, 00h           ; set PIO B OUT to 00fh
-                    out (02h),a         ;
-                    ld a, 03h
-                    call output
-                    ld bc, 01h          ; waiting for power-on-reset
-                    call wait           ; of display finished
-                    call backpack_on
+                    ld hl, 00ff00h      ; begin page to store data
                     ;
-                    ld a, 080h
-                    out (083h), a
-                    ld a, 0ch
-                    out (080h), a
-                    ld a, 00h
+                    ld a, 080h          ; initialize serial interface
+                    out (083h), a       ; enable access to the divisor regs
+                    ld a, 0ch           ; set divisor to 12, causes 9600 
+                    out (080h), a       ; baud rate
+                    ld a, 00h           ; set least significant bit of divisor
                     out (081h), a
-                    ld a, 03h
+                    ld a, 03h           ; set serial mode to 8-N-1
                     out (083h), a
 
-loop:               ld hl, display
-                    call write_frame
-    loop2:          ld a, 01h
-                    call output
-                    ld a, 03h
-                    call output
-                    ;
-                    ld a, 065h
-                    out (080h), a
-                    ld bc, 01h
-                    call wait
-                    ld a, 00ah
-                    out (080h), a
-                    ld bc, 01h
-                    call wait
-                    ld a, 0dh
-                    out (080h), a
-                    ld bc, 0f0h
-                    call wait
-                    ; ld a, 75h
-                    ; out (080h), a
-                    ;
-                    jp loop2            ; just hanging out here for now
+loop:               ld hl, text
+                    call print
+                    jp loop             ; just hanging out here for now
 
-backpack_on:        call startTransmission
-                    ; start oscillator
-                    ld a, 021h
-                    call write
-                    call endTransmission
-                    ; set brightness
-                    call startTransmission
-                    ld a, 0e0h
-                    call write
-                    call endTransmission
-                    ; turn display on
-                    call startTransmission
-                    ld a, 081h
-                    call write
-                    call endTransmission
-                    ret
-
-write_frame:        call startTransmission
-                    ld a, 00h
-                    call write
-                    ld b, 08h
-    lp10:           ld a, (hl)
-                    call write
-                    ld a, 00h
-                    call write
+print:              ld c, (hl)
                     inc hl
-                    dec b
-                    jp nz, lp10
-                    call endTransmission
+                    call serial
+                    sub 00ah
+                    jr nz, print
                     ret
 
-; use terminology similar to Wire.h C-library
-startTransmission:  ld a, 03h
-                    call output
-                    ld bc, 01h      ; this wait is really required
-                    call wait       ; but could probably be shorter
-                    ld a,02h
-                    call output
-                    ld a, 0e0h      ; call device address, move to variable as needed
-                    call write
-                    ret
-
-endTransmission:    ld a, 00h
-                    call output
-                    ld a, 02h
-                    call output
-                    ld a, 03h
-                    call output
-                    ld bc, 01h      ; this wait is required 
-                    call wait       ; but could be shorter
-                    ret
-
-write:              push bc
-                    ld b, 008h      ; for i=0 to 7
-                    ld c, a         ; using c here since I have to push bc anyways
-    lp1:            rlc c           ; rotate to left so that most significant bit becomes least significant
-                    ld a, 001h      ; load mask
-                    and c           ; and deletes all not masked bits
-                    call clbit
-                    dec b
-                    jp nz, lp1
-                    call ackn
-                    pop bc
-                    ret
-
-; clock cycle around bit, from register a
-; removed getting back to original state
-; seems to work without it
-clbit:              push bc
-                    call output
-                    or 002h
-                    call output
-                    and 001h
-                    call output
-                    pop bc
-                    ret
-
-; send acknowledge sequence
-; TODO: actually acknowledge and not
-; just assume ok
-ackn:               
-                    ld a, 001h
-                    call output
-                    ld a, 003h
-                    call output
-                    ret
-
-; simulate open drain by switching 
-; between PIOs output modes
-; see http://members.iinet.net.au/~daveb/downloads/Z80.zip
-output:             ld c, 003h
-                    ld b, 0cfh ; was 0cfh do not remember why
-                    out(c), b
-                    out(c), a
+serial:             in a, (085h)        ; make sure transmitter reg is
+                    and 040h            ; empty
+                    jr z, serial
+                    ld a, c
+                    out (080h), a
                     ret
 
 ; set relative wait time in bc
-wait:               push de         ; protect affected registers
+wait:               push de             ; protect affected registers
                     push af
     outer:          ld de, 0100h
         inner:      dec de
@@ -171,4 +55,4 @@ wait:               push de         ; protect affected registers
                     ret
 
 ; store data here
-display:            db 01eh, 021h, 0d2h, 0c0h, 0d2h, 0cch, 021h, 01eh
+text:               db "I love Cynthia!\r\n"
